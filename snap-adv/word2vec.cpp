ffdea370afc6d5da37bf6c39fd8b624ef12a11d9
Pieter Janin <pieter.janin@ugent.be>
2026-03-24 01:08:44 +0100

    fix word2vec multithreading

--- snap-adv/word2vec.cpp
+++ snap-adv/word2vec.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "Snap.h"
 #include "word2vec.h"
+#include <omp.h>
 
 //Code from https://github.com/nicholas-leonard/word2vec/blob/master/word2vec.c
 //Customized for SNAP and node2vec
@@ -94,25 +95,23 @@ void TrainModel(TVVec<TInt, int64>& WalksVV, const int& Dimensions,
     TIntV& KTable, TFltV& UTable, int64& WordCntAll, TFltV& ExpTable,
     double& Alpha, int64 CurrWalk, TRnd& Rnd,
     TVVec<TFlt, int64>& SynNeg, TVVec<TFlt, int64>& SynPos)  {
-  TFltV Neu1V(Dimensions);
   TFltV Neu1eV(Dimensions);
   int64 AllWords = WalksVV.GetXDim()*WalksVV.GetYDim();
   TIntV WalkV(WalksVV.GetYDim());
   for (int j = 0; j < WalksVV.GetYDim(); j++) { WalkV[j] = WalksVV(CurrWalk,j); }
   for (int64 WordI=0; WordI<WalkV.Len(); WordI++) {
     if ( WordCntAll%10000 == 0 ) {
-      if ( Verbose ) {
-        printf("\rLearning Progress: %.2lf%% ",(double)WordCntAll*100/(double)(Iter*AllWords));
-        fflush(stdout);
+      #pragma omp critical(alpha_update)
+      {
+        if ( Verbose ) {
+          printf("\rLearning Progress: %.2lf%% ",(double)WordCntAll*100/(double)(Iter*AllWords));
+          fflush(stdout);
+        }
+        Alpha = StartAlpha * (1 - WordCntAll / static_cast<double>(Iter * AllWords + 1));
+        if ( Alpha < StartAlpha * 0.0001 ) { Alpha = StartAlpha * 0.0001; }
       }
-      Alpha = StartAlpha * (1 - WordCntAll / static_cast<double>(Iter * AllWords + 1));
-      if ( Alpha < StartAlpha * 0.0001 ) { Alpha = StartAlpha * 0.0001; }
     }
     int64 Word = WalkV[WordI];
-    for (int i = 0; i < Dimensions; i++) {
-      Neu1V[i] = 0;
-      Neu1eV[i] = 0;
-    }
     int Offset = Rnd.GetUniDevInt() % WinSize;
     for (int a = Offset; a < WinSize * 2 + 1 - Offset; a++) {
       if (a == WinSize) { continue; }
@@ -137,8 +136,8 @@ void TrainModel(TVVec<TInt, int64>& WalksVV, const int& Dimensions,
           Product += SynPos(CurrWord,i) * SynNeg(Target,i);
         }
         double Grad;                     //Gradient multiplied by learning rate
-        if (Product > MaxExp) { Grad = (Label - 1) * Alpha; }
-        else if (Product < -MaxExp) { Grad = Label * Alpha; }
+        if (Product >= MaxExp) { Grad = (Label - 1) * Alpha; }
+        else if (Product <= -MaxExp) { Grad = Label * Alpha; }
         else { 
           double Exp = ExpTable[static_cast<int>(Product*ExpTablePrecision)+TableSize/2];
           Grad = (Label - 1 + 1 / (1 + Exp)) * Alpha;
@@ -152,6 +151,7 @@ void TrainModel(TVVec<TInt, int64>& WalksVV, const int& Dimensions,
         SynPos(CurrWord,i) += Neu1eV[i];
       }
     }
+    #pragma omp atomic
     WordCntAll++;
   }
 }
@@ -164,7 +164,7 @@ void LearnEmbeddings(TVVec<TInt, int64>& WalksVV, const int& Dimensions,
   TIntIntH RnmBackH;
   int64 NNodes = 0;
   //renaming nodes into consecutive numbers
-  for (int i = 0; i < WalksVV.GetXDim(); i++) {
+  for (int64 i = 0; i < WalksVV.GetXDim(); i++) {
     for (int64 j = 0; j < WalksVV.GetYDim(); j++) {
       if ( RnmH.IsKey(WalksVV(i, j)) ) {
         WalksVV(i, j) = RnmH.GetDat(WalksVV(i, j));
@@ -193,13 +193,18 @@ void LearnEmbeddings(TVVec<TInt, int64>& WalksVV, const int& Dimensions,
     ExpTable[i] = TMath::Power(TMath::E, Value);
   }
   int64 WordCntAll = 0;
+  int Seed = Rnd.GetSeed();
 // op RS 2016/09/26, collapse does not compile on Mac OS X
 //#pragma omp parallel for schedule(dynamic) collapse(2)
   for (int j = 0; j < Iter; j++) {
-#pragma omp parallel for schedule(dynamic)
-    for (int64 i = 0; i < WalksVV.GetXDim(); i++) {
-      TrainModel(WalksVV, Dimensions, WinSize, Iter, Verbose, KTable, UTable,
-       WordCntAll, ExpTable, Alpha, i, Rnd, SynNeg, SynPos); 
+#pragma omp parallel
+    {
+      TRnd LocalRnd(Seed + omp_get_thread_num());
+#pragma omp for schedule(dynamic)
+      for (int64 i = 0; i < WalksVV.GetXDim(); i++) {
+        TrainModel(WalksVV, Dimensions, WinSize, Iter, Verbose, KTable, UTable,
+         WordCntAll, ExpTable, Alpha, i, LocalRnd, SynNeg, SynPos); 
+      }
     }
   }
   if (Verbose) { printf("\n"); fflush(stdout); }
@@ -209,3 +214,4 @@ void LearnEmbeddings(TVVec<TInt, int64>& WalksVV, const int& Dimensions,
     EmbeddingsHV.AddDat(RnmBackH.GetDat(i), CurrV);
   }
 }
+
